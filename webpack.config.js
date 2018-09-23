59c58d0fd0e7a6da190ce75b242ae12e69e4918f
Jack11709 <jackmay89@live.com>
2018-09-23 12:38:14 +0100

    added file loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,54 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: './src/index.html',
-    filename: './index.html'
+  template: './src/index.html',
+  filename: './index.html'
 });
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                include: ['/styles'],
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.css$/,
-                exclude: ['/styles'],
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[name]_[local]_[hash:base64]',
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        include: ['/styles'],
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.css$/,
+        exclude: ['/styles'],
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
             }
+          }
         ]
-    },
-    plugins: [htmlPlugin],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 4000
-    }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000
+  }
 };
